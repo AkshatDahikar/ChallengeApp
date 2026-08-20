50fe9beb70d5d84ec3eb5294b5c799d6b8d8988a
AkshatDahikar <akshatdahikar@gmail.com>
2026-08-20 12:05:21 +0530

    Completed ChallengeApp Project

--- src/main/java/com/firstprojectexample/ChallengesApp/ChallengeController.java
+++ src/main/java/com/firstprojectexample/ChallengesApp/ChallengeController.java
@@ -1,35 +1,59 @@
 package com.firstprojectexample.ChallengesApp;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/challenges")
 public class ChallengeController {
     private ChallengeService challengeService;
     public ChallengeController(ChallengeService challengeService){
         this.challengeService = challengeService;
     }
-    @GetMapping("/challenges")
-    public List<Challenge> getAllChallenges(){
-        return challengeService.getAllChallenges();
+    @GetMapping
+    public  ResponseEntity<List<Challenge>> getAllChallenges(){
+        return new ResponseEntity<>(challengeService.getAllChallenges(),HttpStatus.OK);
 }
-@PostMapping("/challenges")
-public String addChallenge( @RequestBody Challenge challenge){
+@PostMapping
+public ResponseEntity<String> addChallenge(
+        @RequestBody Challenge challenge){
   boolean isChallengeAdded =
           challengeService.addChallenge(challenge);
   if (isChallengeAdded)
-    return "Challenge added successfully";
+    return new ResponseEntity("Challenge added successfully",HttpStatus.OK);
   else
-      return "Challenge not added successfully";
+      return new ResponseEntity("Challenge not added successfully",
+              HttpStatus.NOT_FOUND);
 }
     @GetMapping("/challenges/{month}")
-    public Challenge getChallenge(@PathVariable String month){
+    public ResponseEntity<Challenge> getChallenge(
+            @PathVariable String month){
          Challenge challenge =
                  challengeService.getChallenge(month);
          if(challenge != null)
-             return challenge;
+             return new ResponseEntity<>(challenge, HttpStatus.OK);
          else
-             return null;
+             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateChallenge(@PathVariable Long id,@RequestBody Challenge updatedChallenge){
+     boolean isChallengeUpdated = challengeService.updateChallenge(id,updatedChallenge);
+        if (isChallengeUpdated)
+            return new ResponseEntity("Challenge updated successfully",HttpStatus.OK);
+        else
+            return new ResponseEntity("Challenge not updated successfully",
+                    HttpStatus.NOT_FOUND);
+    }
+        @DeleteMapping("/{id}")
+        public ResponseEntity<String> deleteChallenge(@PathVariable Long id){
+            boolean isChallengeDeleted = challengeService.deleteChallenge(id);
+            if (isChallengeDeleted)
+                return new ResponseEntity("Challenge deleted successfully",HttpStatus.OK);
+            else
+                return new ResponseEntity("Challenge not deleted successfully",
+                        HttpStatus.NOT_FOUND);
+        }
 }
